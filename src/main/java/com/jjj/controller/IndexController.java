d9334c571a055fd12ab304ca44ddad827efe6521
joelib <a6691893@163.com>
2022-09-28 00:08:25 +0800

    day3-部署ing

--- src/main/java/com/jjj/controller/IndexController.java
+++ src/main/java/com/jjj/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.jjj.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @className: com.jjj.controller.Controller
  * @description:
  * @author: 江骏杰
  * @create: 2022-09-24 20:54
  */
-public class Controller {
+
+@Controller
+public class IndexController {
 }
