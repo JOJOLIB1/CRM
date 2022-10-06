2c45c249e5851ea4d999fe6748b3f3563bcd87e1
joelib <a6691893@163.com>
2022-10-06 23:34:36 +0800

    导航ing

--- src/main/java/com/jjj/crm/workbench/controller/ActivityController.java
+++ src/main/java/com/jjj/crm/workbench/controller/ActivityController.java
@@ -10,10 +10,7 @@ import com.jjj.crm.workbench.pojo.Activity;
 import com.jjj.crm.workbench.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
@@ -86,9 +83,9 @@ public class ActivityController {
         return returnMsg;
     }
 
-    @GetMapping("/workbench/activity/query")
+    @GetMapping("/workbench/activity/query/{pageNo}")
     @ResponseBody
-    public Object query(Integer pageSize, Integer pageNo, Activity condition) {
+    public Object query(Integer pageSize, @PathVariable("pageNo") Integer pageNo, Activity condition) {
         return activityService.queryActivitiesForPages(pageSize, pageNo, condition);
     }
 
